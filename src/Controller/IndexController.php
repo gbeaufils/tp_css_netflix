ec671bde0a8366cc7daa3649ccb08dc0e49df027
Geoffroy Beaufils <g.beaufils@attineos.com>
2020-06-25 14:47:36 +0200

    ajout

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\Omdb;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -28,12 +29,37 @@ class IndexController extends AbstractController
     }
 
     /**
-      * @Route("/catalogue", name="catalogue")
+      * @Route("/catalogue/{currentPage}", name="catalogue", defaults={"currentPage"=1})
       */
-     public function catalogue()
+     public function catalogue(int $currentPage = 1)
      {
+         $omdbApi = new Omdb();
+         $listeFilms = [];
+         $listeFilms['films'] = [];
+
+         // Fake data : Id de films pour l'affichage dans catalogue
+         $dataFilmsFavoris = ['tt0903747', 'tt1375666', 'tt0944947','tt0121955','tt4574334', 'tt2306299', 'tt5727474',
+             'tt2527336', 'tt4052886', 'tt1386697'];
+
+         $dataFilmsFavoris2 = ['tt5180504', 'tt0108778', 'tt1632701', 'tt4154796',
+             'tt2935510', 'tt4532826', 'tt1365519', 'tt2873282', 'tt4972582', 'tt0803096'];
+
+         $fakeDataPage = $currentPage == 1 ? $dataFilmsFavoris : $dataFilmsFavoris2;
+
+         $listeFilms['response'] = 'True';
+         $listeFilms['nbPages'] = 2;
+         $listeFilms['currentPage'] = $currentPage;
+
+         foreach ($fakeDataPage as $idFilm)
+         {
+             $dataOmdbApi = $omdbApi->getById($idFilm);
+
+             array_push($listeFilms['films'], $dataOmdbApi);
+         }
+
          return $this->render('index/catalogue.html.twig', [
              'controller_name' => 'IndexController',
+             'resultRecherche' => $listeFilms
          ]);
      }
 }
